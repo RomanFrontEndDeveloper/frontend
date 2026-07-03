f4e5a00e9e40937518ebd515f1012ee0a1fc41f2
RomanFronrEnd <romariotraveler@gmail.com>
2026-07-03 17:00:36 +0300

    Initial frontend setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<main className='flex flex-1 items-center justify-center'>
+			<div className='text-center'>
+				<h1 className='text-5xl font-bold'>FreelanceHub</h1>
+				<p className='mt-4 text-gray-500'>Welcome to FreelanceHub!</p>
+			</div>
+		</main>
+	);
+}
